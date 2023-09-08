7ac9118babef0bd208f141fb76e2d81319421ce3
KRKYYDS <71657787+KRK11@users.noreply.github.com>
2023-09-08 14:04:01 +0800

    Update settings.py
    修改信息。

--- Design/settings.py
+++ Design/settings.py
@@ -3,8 +3,6 @@ BOT_NAME = 'Design'
 SPIDER_MODULES = ['Design.spiders']
 NEWSPIDER_MODULE = 'Design.spiders'
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
-
 ROBOTSTXT_OBEY = False
 
 CONCURRENT_REQUESTS = 32
